2c23e2b483d4187b0832d5fe84868747ba338271
banhiKU04 <155135807+banhiKU04@users.noreply.github.com>
2024-02-11 22:32:45 +0600

    Cell design

--- Documents/NetBeansProjects/Calendar-Custom/src/main/java/main/Cell.java
+++ Documents/NetBeansProjects/Calendar-Custom/src/main/java/main/Cell.java
@@ -1,10 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.Date;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -14,22 +13,27 @@ public class Cell extends JButton {
     
     private Date date;
     private boolean title;
+    private boolean isToDay;
+    
     public Cell(){
        setContentAreaFilled(false);
        setBorder(null);
        setHorizontalAlignment(JLabel.CENTER);
     }
+    
     public void asTitle()
     {
         title = true;
     }
+    
     public boolean isTitle(){
         return title;
     }
-    public void setDate(Date date)
-    {
+    
+    public void setDate(Date date){
         this.date = date;
     }
+    
     public void currentMonth(boolean act){
         if(act){
             setForeground(new Color(68,68,68));
@@ -38,6 +42,29 @@ public class Cell extends JButton {
             setForeground(new Color(170,170,170));
         }
     }
+
+    public void setAsToDay(){
+        isToDay = true;
+        setForeground(Color.WHITE);
+    }
+
+    @Override
+    protected void paintComponent(Graphics grphcs){
+        if(title){
+            grphcs.setColor(new Color(213,213,213));
+            grphcs.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
+        }
+
+        if(isToday)
+        {
+            Graphics2D g2 = (Graphics2D) grphcs;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(new Color(97, 49,237));
+            int x = getWidth() / 2 - 17;
+            int y = getHeight() / 2 - 17;
+            g2.fillRoundRect(x, y, 35, 35, 100, 100);
+        }
+        super .paintComponent(grphcs);
+    }
     
     
-}
